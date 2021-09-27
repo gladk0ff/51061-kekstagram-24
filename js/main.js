ff22bbefbed29018bb779d4179fffe08ff38be5d
gladkoff <agladkov@logrocon.com>
2021-09-27 20:09:22 +0300

    add functions

---
+++ js/main.js
@@ -0,0 +1,23 @@
+//Функция, возвращающая случайное целое число из переданного диапазона включительно.
+function getRandomFromInterval(min, max) {
+  if (max < 0 || min < 0) {
+    console.error('диапазон может быть только положительный, включая ноль.');
+    return;
+  }
+  if (max <= min) {
+    console.error('переметры заданы неверно : max > min');
+    return;
+  }
+  return Math.floor(Math.random() * (max - min) + min);
+}
+getRandomFromInterval(1, 7);
+
+//Функция для проверки максимальной длины строки.
+function hasStringLength(str, limit) {
+  if (!str || !limit) {
+    console.error('Не заданы параметры функции');
+  }
+
+  return str.length <= limit;
+}
+hasStringLength('Ололо', 3);
